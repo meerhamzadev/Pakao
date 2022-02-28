8f31b53cdc30a84e14d89034b4daf3f723d2ba05
meerhamzadev <hamzababar37@gmail.com>
2022-02-28 15:10:31 +0500

    📦New: Add firebase authentication

--- lib/Authentication/login.dart
+++ lib/Authentication/login.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:pakao/Home/home_page.dart';
+import '../Services/auth.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -9,76 +11,88 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   final _formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  AuthService _auth = AuthService();
   @override
   Widget build(BuildContext context) {
-    return Form(
-        key: _formKey,
-        child: Column(
-          children: [
-            Icon(
-              Icons.account_circle_rounded,
-              color: Colors.grey[400],
-              size: 100,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                validator: (String? value) {
-                  if (value == null || value!.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  labelText: 'Email',
-                  hintText: 'Enter your email',
-                  prefixIcon: const Icon(Icons.email),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+    return Scaffold(
+      body: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Icon(
+                Icons.account_circle_rounded,
+                color: Colors.grey[400],
+                size: 100,
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) {
+                    setState(() => email = value);
+                  },
+                  decoration: InputDecoration(
+                    labelText: 'Email',
+                    hintText: 'Enter your email',
+                    prefixIcon: const Icon(Icons.email),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: TextFormField(
-                validator: (value) {
-                  if (value == null || value!.isEmpty) {
-                    return 'Please enter some text';
-                  }
-                  return null;
-                },
-                decoration: InputDecoration(
-                  labelText: 'Password',
-                  hintText: 'Enter your password',
-                  prefixIcon: const Icon(Icons.vpn_key_rounded),
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10.0),
+              Padding(
+                padding: const EdgeInsets.all(10),
+                child: TextFormField(
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter some text';
+                    }
+                    return null;
+                  },
+                  onChanged: (value) => {setState(() => password = value)},
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    hintText: 'Enter your password',
+                    prefixIcon: const Icon(Icons.vpn_key_rounded),
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 40,
-              width: 480,
-              child: ElevatedButton(
-                child: const Text('Login'),
-                style: ButtonStyle(
-                    backgroundColor:
-                        MaterialStateProperty.all<Color>(Colors.orange)),
-                onPressed: () {
-                  if (_formKey.currentState!.validate()) {
-                    // ScaffoldMessenger.of(context).showSnackBar(
-                    //   const SnackBar(content: Text('Processing Data')),
-                    // );
-                    _formKey.currentState!.save();
-                    Navigator.pushNamed(context, '/login');
-                  }
-                  ;
-                },
+              SizedBox(
+                height: 40,
+                width: 480,
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  style: ButtonStyle(
+                      backgroundColor:
+                          MaterialStateProperty.all<Color>(Colors.orange)),
+                  onPressed: () async {
+                    if (_formKey.currentState!.validate()) {
+                      _formKey.currentState!.save();
+                      dynamic result = await _auth.Login(email, password);
+                      if (result == null) {
+                        Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => HomePage()));
+                      }
+                    }
+                    ;
+                  },
+                ),
               ),
-            ),
-          ],
-        ));
+            ],
+          )),
+    );
   }
 }
